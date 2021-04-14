06f7f45edccdecc45907bbdeb32c0e898b7072c4
DangToan <dtoan2805@gmail.com>
2021-04-13 17:57:52 -0700

    Add Account Controller in MvcClient

---
+++ MvcClient/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "oidc");
+        }
+
+        public new IActionResult SignOut()
+        {
+            return SignOut(new AuthenticationProperties { RedirectUri = "/" }, "Cookies", "oidc");
+        }
+    }
+}
