398320d231c6d6c05b85129d6d0ee39e1948fc1a
DangToan <dtoan2805@gmail.com>
2021-04-13 17:56:40 -0700

    Update host config

--- CoreApi/Areas/Identity/IdentityHostingStartup.cs
+++ CoreApi/Areas/Identity/IdentityHostingStartup.cs
@@ -1,5 +1,4 @@
 using System;
-using Core.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -14,13 +13,8 @@ namespace CoreApi.Areas.Identity
     {
         public void Configure(IWebHostBuilder builder)
         {
-            builder.ConfigureServices((context, services) => {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlServer(
-                        context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
-
-                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.ConfigureServices((context, services) =>
+            {
             });
         }
     }
--- CoreApi/Config.cs
+++ CoreApi/Config.cs
@@ -41,14 +41,14 @@ namespace CoreApi
                 // interactive client using code flow + pkce
                 new Client
                 {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("serect".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.Code,
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:6001/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:6001/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:6001/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
                     AllowedScopes = { "openid", "profile", "scope2" }
